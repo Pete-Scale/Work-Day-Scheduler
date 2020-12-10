ed45698e8dc15cdd45ce96661b09460cb31d7655
Pete-Scale <petescale@gmail.com>
2020-12-09 18:36:32 -0600

    added array of time-block objects

--- script.js
+++ script.js
@@ -9,3 +9,61 @@ function headerDate() {
 
 // Loads today's date
 headerDate();
+
+// Array of time-block objects
+var schedule = [
+    {
+        id: "9",
+        hour: "09",
+        meridiem: "am",
+        note: ""
+    },
+    {
+        id: "10",
+        hour: "10",
+        meridiem: "am",
+        note: ""
+    },
+    {
+        id: "11",
+        hour: "11",
+        meridiem: "am",
+        note: ""
+    },
+    {
+        id: "12",
+        hour: "12",
+        meridiem: "pm",
+        note: ""
+    },
+    {
+        id: "13",
+        hour: "01",
+        meridiem: "pm",
+        note: ""
+    },
+    {
+        id: "14",
+        hour: "02",
+        meridiem: "pm",
+        note: ""
+    },
+    {
+        id: "15",
+        hour: "03",
+        meridiem: "pm",
+        note: ""
+    },
+    {
+        id: "16",
+        hour: "04",
+        meridiem: "pm",
+        note: ""
+    },
+    {
+        id: "17",
+        hour: "05",
+        meridiem: "pm",
+        note: ""
+    },
+];
